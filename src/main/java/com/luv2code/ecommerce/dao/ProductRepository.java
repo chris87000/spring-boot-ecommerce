f7db8707fa08914eb450be0e47f6dac838eea5a1
Chris Schmitt <christian.onweb87@gmail.com>
2022-07-20 00:28:43 +0200

    commit du soir

---
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.luv2code.ecommerce.dao;
+
+import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "book", path="books")
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+
+}
